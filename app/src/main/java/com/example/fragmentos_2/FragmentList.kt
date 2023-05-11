b9134f90037541880faaa8ba0a52f2cc9492d78b
Jose Francisco Luna <pacolunahdz@hotmail.com>
2023-05-10 19:26:50 -0600

    Se termina el fragment list

---
+++ app/src/main/java/com/example/fragmentos_2/FragmentList.kt
@@ -0,0 +1,35 @@
+package com.example.fragmentos_2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+
+
+class FragmentList : Fragment() {
+
+    private lateinit var lista: ListView
+    private val so = arrayOf(
+        "Windows", "MacOS", "Linux", "Debian", "Android", "Solaris", "RedHat",
+        "Fedora", "CentOS", "Ubuntu", "Kali", "Tiles", "iOS", "Mint", "Windows Phone"
+    )
+    private val list: ArrayList<String> = ArrayList(so.toList())
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view: View = inflater.inflate(R.layout.fragment_list, container, false)
+        lista = view.findViewById(R.id.main_activity_lista)
+        val adapter = ArrayAdapter(
+            requireActivity(),
+            android.R.layout.simple_list_item_1, list
+        )
+        lista.setAdapter(adapter);
+        return view
+    }
+}
